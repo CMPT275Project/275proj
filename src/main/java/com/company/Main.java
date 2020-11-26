44bfbd921baeccaf26165c2ec4bfcff540da122d
CMPT275Project <1090592163@qq.com>
2020-11-25 16:10:20 -0800

    upload_With_Maven

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("testtestt02222000est");
+        //registerController testReg = new registerController();
+        //testReg.addUserEmail(123,"asd","asd","","","");
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://34.83.219.17:3306/connect","root","root");
+            //here sonoo is database name, root is username and password
+            Statement stmt = con.createStatement();
+            String sql = "DELETE FROM userLogin WHERE id = 0";
+            //String sql2 = "INSERT INTO userLogin VALUES ('qweqwe', 'qweqwe');";
+            stmt.executeUpdate(sql);
+            //stmt.executeUpdate(sql2);
+
+            ResultSet rs = stmt.executeQuery("select * from userLogin");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2));
+            con.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        //*/
+    }
+
+
+
+}
