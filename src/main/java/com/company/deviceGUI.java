f3069a2b602310fa0e446bba7afc660a84125a63
Ethan <c68830176ys@gmail.com>
2020-12-03 18:34:25 -0800

    updated

--- src/main/java/com/company/deviceGUI.java
+++ src/main/java/com/company/deviceGUI.java
@@ -3,6 +3,7 @@ package com.company;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -48,10 +49,12 @@ public class deviceGUI extends Component implements ActionListener {
         rental_pan.setLayout(null);
         account_pan = new JPanel();
         account_pan.setLayout(null);
-
+        
         tp.addTab("a",device_pan);
         tp.addTab("b", rental_pan);
         tp.addTab("c", account_pan);
+       
+        
         frame.add(tp);
 
         // Panel for device
@@ -61,13 +64,35 @@ public class deviceGUI extends Component implements ActionListener {
         //device_pan.add(user_label);
 
 
-        String[] col = {"Name", "ID", "email"};
-        String[][] row = {{"hi", "001", "a@gmail.com"} , {"jj", "002", "ppp"}, {"jj", "002", "ppp"},{"jj", "002", "ppp"}};
+        String[] col = {"Name", "ID", "email","Name", "ID", "email","Name", "ID", "email"};
+        String[][] row = {{"hifref", "001", "a@gmail.com","hifref", "001", "a@gmail.com","hifref", "001", "a@gmail.com"} , 
+        				  {"jjdw", "002", "ppp","jjdw", "002", "ppp","jjdw", "002", "ppp"}, 
+        				  {"jjdwdw", "002", "ppp","jjdwdw", "002", "ppp","jjdwdw", "002", "ppp"},
+        				  {"jjdw", "002", "ppp","jjdw", "002", "ppp","jjdw", "002", "ppp"},
+        				  {"jjdw", "002", "ppp","jjdw", "002", "ppp","jjdw", "002", "ppp"},
+        				  {"ferwgr", "002", "ppp","jjdwedw", "002", "ppp","jjdwedw", "002", "ppp"},
+        				  {"ferwgr", "002", "ppp","jjdwedw", "002", "ppp","jjdwedw", "002", "ppp"},
+        				  {"ferwgr", "002", "ppp","jjdwedw", "002", "ppp","jjdwedw", "002", "ppp"}};
 
 
         // not working
-        DefaultTableModel model = new DefaultTableModel();
-        device_table = new JTable(row,col);
+//        DefaultTableModel model = new DefaultTableModel();
+//        device_table = new JTable(row,col);
+        device_table = new JTable();
+        device_table.setEnabled(false);
+        device_table.setModel(new DefaultTableModel(row,col) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+        
+        JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(50,100,700,300);
+		scrollPane.setViewportView(device_table);
+		device_pan.add(scrollPane);
         //scroll_pane = new JScrollPane(device_pan);
         //scroll_pane.setVisible(true);
 
@@ -75,7 +100,7 @@ public class deviceGUI extends Component implements ActionListener {
         device_table.setPreferredScrollableViewportSize(new Dimension(100,200));
         device_table.setBounds(400,50,450,50);
         device_table.setFillsViewportHeight(true);
-        device_table.setRowHeight(30);
+        device_table.setRowHeight(72);
         device_table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         device_table.setAutoCreateRowSorter(true);
 
